e4f8df3176c5cffc0893512427f14fadd6c48eb6
iconoclast hero <iconoclasthero@gmail.com>
2026-06-15 20:59:29 -0400

    building production Mon Jun 15 08:59:29 PM EDT 2026

--- src/components/AlbumArt.vue
+++ src/components/AlbumArt.vue
@@ -41,8 +41,16 @@
       target="_blank"
       title="Search cover art"
     >
-      🔍
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-binoculars-fill" viewBox="0 0 16 16">
+  <path d="M4.5 1A1.5 1.5 0 0 0 3 2.5V3h4v-.5A1.5 1.5 0 0 0 5.5 1zM7 4v1h2V4h4v.882a.5.5 0 0 0 .276.447l.895.447A1.5 1.5 0 0 1 15 7.118V13H9v-1.5a.5.5 0 0 1 .146-.354l.854-.853V9.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v.793l.854.853A.5.5 0 0 1 7 11.5V13H1V7.118a1.5 1.5 0 0 1 .83-1.342l.894-.447A.5.5 0 0 0 3 4.882V4zM1 14v.5A1.5 1.5 0 0 0 2.5 16h3A1.5 1.5 0 0 0 7 14.5V14zm8 0v.5a1.5 1.5 0 0 0 1.5 1.5h3a1.5 1.5 0 0 0 1.5-1.5V14zm4-11H9v-.5A1.5 1.5 0 0 1 10.5 1h1A1.5 1.5 0 0 1 13 2.5z"/>
+</svg>
     </a>
+    <a href="#"
+      class="cover-zoom-button"
+      @click="$epit('action', 'zoom')"
+      title="Zoom image"
+    >🔍</a>
+
 
      <a href="#"
        v-if="albumArtData"
@@ -50,6 +58,7 @@
        @click="$emit('action', 'fix_art')"
        title="Mark album art for fixing"
      >⚠</a>
+
 </div>
     <!-- Placeholder / fallback -->
 
@@ -316,7 +325,7 @@ watch([() => props.artist, () => props.mbArtistID], ([artist, mbid]) => {
   line-height: 1;
 }
 
-.fix-art-button {
+.cover-zoom-button {
   position: absolute;
   right: 6px;
   bottom: 6px;
@@ -325,9 +334,8 @@ watch([() => props.artist, () => props.mbArtistID], ([artist, mbid]) => {
 
 .fix-art-button {
   position: absolute;
-  right: 4px;
-  bottom: 4px;
-
+  left: 6px;
+  bottom: 6px;
   z-index: 10;
 }
 
