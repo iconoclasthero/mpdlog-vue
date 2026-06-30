600f28027aa998466f88cc0173375838c6832e70
iconoclast hero <iconoclasthero@gmail.com>
2026-06-29 20:34:12 -0400

    building production Mon Jun 29 08:34:12 PM EDT 2026

--- src/components/AlbumArt.vue
+++ src/components/AlbumArt.vue
@@ -8,34 +8,13 @@
     </svg>
     <!-- Desktop: unchanged -->
 
-<!--
-    <img
-      v-if="albumArtData"
-      :src="albumArtData"
-      alt="Album Art"
-      @click="$emit('refreshArt')"
-      :style="{ height: 'auto', maxWidth: layout.narrow.value ? mobileMaxWidth : deskWidth + 'px' }"
-    >
--->
 <div v-if="albumArtData" class="art-block">
-<!--
-    <img
-      v-if="albumArtData"
-      :src="albumArtData"
-      alt="Album Art"
-      @click="$emit('refreshArt')"
-      :style="imgStyle"
-      @mouseenter="hovered = true"
-      @mouseleave="hovered = false"
-      @load="checkSize"
-    >
--->
     <img
       v-if="albumArtData"
       :src="albumArtData"
       alt="Album Art"
       @click="handleImageClick"
-      :style="imgStyle"
+      :style="[imgStyle, isLowRes ? { border: '3px solid red', boxSizing: 'border-box' } : {}]"
       @load="checkSize"
     >
 
@@ -46,11 +25,6 @@
       target="_blank"
       title="Search cover art"
     >
-<!--  This was moved to the symbol definition at the top <div> Sun Jun 21 2026.  Remove the next time you see it.
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-binoculars-fill" viewBox="0 0 16 16">
-  <path d="M4.5 1A1.5 1.5 0 0 0 3 2.5V3h4v-.5A1.5 1.5 0 0 0 5.5 1zM7 4v1h2V4h4v.882a.5.5 0 0 0 .276.447l.895.447A1.5 1.5 0 0 1 15 7.118V13H9v-1.5a.5.5 0 0 1 .146-.354l.854-.853V9.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v.793l.854.853A.5.5 0 0 1 7 11.5V13H1V7.118a1.5 1.5 0 0 1 .83-1.342l.894-.447A.5.5 0 0 0 3 4.882V4zM1 14v.5A1.5 1.5 0 0 0 2.5 16h3A1.5 1.5 0 0 0 7 14.5V14zm8 0v.5a1.5 1.5 0 0 0 1.5 1.5h3a1.5 1.5 0 0 0 1.5-1.5V14zm4-11H9v-.5A1.5 1.5 0 0 1 10.5 1h1A1.5 1.5 0 0 1 13 2.5z"/>
-</svg>
--->
       <svg width="16" height="16">
         <use href="#binoculars-icon" />
       </svg>
@@ -138,6 +112,8 @@ const emit = defineEmits(['refreshArt', 'action'])
 
 // const hovered = ref(false)
 
+const isLowRes = ref(false)
+
 const props = defineProps({
   albumArtData: String,
   artist: String,
@@ -205,13 +181,16 @@ const checkSize = (ev) => {
 //    coverSearchURL.value
 //  )
 
+  isLowRes.value = img.naturalWidth < 1200 || img.naturalHeight < 1200
+
   console.log(
     `[AlbumArt] Dimensions: %c${img.naturalWidth} × ${img.naturalHeight}%c\n\
      ${props.current.artist} -- ${props.current.album}\n\
-     ${coverSearchURL.value}`,
+     ${coverSearchURL.value}\n isLowRes: ${isLowRes.value}\n`,
     'color: #b392f0; font-weight: bold;', // Style for the dimensions
     'color: inherit; font-weight: normal;' // Reset style for the rest of the text
   )
+
 
   if ( false && (
     img.naturalWidth <= 500 ||
