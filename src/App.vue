dc819b396c8ba8097e3cb1972a1a9763258f1ec9
iconoclast hero <iconoclasthero@gmail.com>
2026-08-03 11:28:00 -0400

    building production Mon Aug  3 11:28:00 AM EDT 2026

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
       :color="ringColor"
       :playing="player.state==='play'"
       :playerStatusUpdate="playerStatusUpdate"
-      img-src="/Days Between transparent-96x96.png"
+      img-src="/Days Between transparent-96x96-2.png"
       @seek="seekTo"
       @action="handleAction"
     />
